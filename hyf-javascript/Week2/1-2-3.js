79755833069c6ff584d7caab24cc5f999de29c7e
ShaimaaElbanna <ShaimaaElbanna@users.noreply.github.com>
2018-10-14 11:36:39 +0200

    ok

--- hyf-javascript/Week2/1-2-3.js
+++ hyf-javascript/Week2/1-2-3.js
@@ -1,18 +1,20 @@
-//1
+//1 Create a function that takes 3 arguments and returns the sum of the these arguments
 function sum(a,b,c){
-    const result = a + b + c;
-    return result;
+    return a + b + c
 }
-console.log (sum(5,6,10));
+console.log(sum(7,8,9));
 
-//2
-function colorCar(a){
-    const result = "a " + a + " car"
-    return result;
+
+//2 Create a function named colorCar that receives a color, 
+//and prints out, 'a red car' for example.
+function colorCar(color){
+  return  "a " + color + " car"
 }
 console.log(colorCar("blue"));
 
-//3
+//3 Create an object and a function that takes the object as a parameter
+// and prints out all of its properties and values.
+
 
 const myobject = {      
     prop1: "cat",
